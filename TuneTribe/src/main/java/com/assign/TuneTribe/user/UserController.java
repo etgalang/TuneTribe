a74f4df3b01ceece1e5dc84e8c7c304bcc04f01d
csc340sp24 <M_GARCIAPAE@uncg.edu>
2024-04-25 16:31:03 -0400

    Worked on more html pages and main page

--- TuneTribe/src/main/java/com/assign/TuneTribe/user/UserController.java
+++ TuneTribe/src/main/java/com/assign/TuneTribe/user/UserController.java
@@ -1,6 +1,8 @@
 
 package com.assign.TuneTribe.user;
 
+import com.assign.TuneTribe.post.PostService;
+import com.assign.TuneTribe.song.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,14 +20,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     
-    public static String currUser = null;
+    public static String currUser="";
+    
+    @Autowired 
+    private UserServ service;
     
     @Autowired
-    private UserServ service;
+    private PostService pService;
+    
+    @Autowired
+    private SongService sService;
     
     @GetMapping({"/", "","/home"}) //we need user info and post info here
     public String homePage (Model model) {
-        model.addAttribute("user", service.getUser(currUser));
+        model.addAttribute("user", service.getUser(currUser)); //add multiple attributes to a model?
+        model.addAttribute("postList", pService.getAllPosts());
+        model.addAttribute("songList", sService.getAllSongs());
         return "user/user-home";
     }
   
@@ -42,7 +52,7 @@ public class UserController {
     
     @GetMapping("/myProfile={username}")
     public String myProfile(@PathVariable String username,Model model){
-        if(currUser == null){
+        if(currUser.isEmpty()){
             currUser = username;
         }
         else {
