90dcef5bff5bf0e8dfe717a3a32ff934bfe825dd
shaunaayscue <156261506+shaunaayscue@users.noreply.github.com>
2024-04-27 09:50:22 -0400

    edited from github file

--- TuneTribe/src/main/java/com/assign/TuneTribe/admin/AdminRepository.java
+++ TuneTribe/src/main/java/com/assign/TuneTribe/admin/AdminRepository.java
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package com.assign.TuneTribe.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-=======
 package com.assign.TuneTribe.admin;
 
 
@@ -19,16 +8,11 @@ import org.springframework.stereotype.Repository;
 
 import com.assign.TuneTribe.user.User;
 
->>>>>>> fbe07ab22d812ffa7f86708cc768a82fe22b6298
 /**
  *
  * @author shauna
  */
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-<<<<<<< HEAD
-    
-=======
     Optional<Admin> findByUser(User adminUser);
->>>>>>> fbe07ab22d812ffa7f86708cc768a82fe22b6298
 }
