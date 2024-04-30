153a00730a486c9d85d6a77e4d9da05452c3a217
csc340sp24 <M_GARCIAPAE@uncg.edu>
2024-04-29 21:32:28 -0400

    fixed artist and mod redirect after login

--- TuneTribe/src/main/java/com/assign/TuneTribe/AppController.java
+++ TuneTribe/src/main/java/com/assign/TuneTribe/AppController.java
@@ -31,10 +31,10 @@ public class AppController {
 
     @Autowired
     UserService service;
-    
+
     @Autowired
     TopSongsService tpService;
-    
+
     @Autowired
     private AdminService adminService;
 
@@ -46,16 +46,22 @@ public class AppController {
             // Check if the user has the admin role
             if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Admin"))) {
                 long totalUsers = adminService.getTotalUsers();
-        model.addAttribute("totalUsers", totalUsers);
-        
+                model.addAttribute("totalUsers", totalUsers);
+
                 return "admin/admin"; // Redirect admin users to admin page
+            } else if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Artist"))) {
+
+                return "redirect:/artist/home";
+            } else if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Mod"))) {
+
+                return "redirect:/mod/mod";
             } else {
                 // Regular users go to root page
                 String name = auth.getName();
                 model.addAttribute("currentUser", name); //returns username
                 System.out.println("Details " + auth.getDetails());//
-                System.out.println("Authorities /'" + auth.getAuthorities().toString()+"/'");//
-                System.out.println("Authorities /'" + auth.getAuthorities()+"/'");//
+                System.out.println("Authorities /'" + auth.getAuthorities().toString() + "/'");//
+                System.out.println("Authorities /'" + auth.getAuthorities() + "/'");//
                 return "redirect:/user/home";
             }
         }
@@ -64,10 +70,10 @@ public class AppController {
 
     @GetMapping(value = {"/admin/home"})
     public String admin(Model model) {
-         long totalUsers = adminService.getTotalUsers();
+        long totalUsers = adminService.getTotalUsers();
         model.addAttribute("totalUsers", totalUsers);
-        
-    return "admin/admin";
+
+        return "admin/admin";
     }
 
     @GetMapping("/login")
@@ -87,5 +93,4 @@ public class AppController {
         return "redirect:/login";
     }
 
-   
 }
